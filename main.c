339dcbd33ccc98d4ce59c9de94afa14ddcca6566
Zisis Beligiannis <bljhn024@gmail.com>
2018-12-02 00:52:09 +0200

    part_2 - commit 1

--- main.c
+++ main.c
@@ -4,14 +4,26 @@
 #include <math.h>
 #include <time.h>
 
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
 #include "str.h"
 #include "result.h"
+
+#include "utils.h"
+
+
+
+#define PATH "/home/zisis/Desktop/submission/submission/workloads/small/"
 
 int main(void){
 
 	//test_case_0
 	//Table R
-	int r_size = 10;
+/*	int r_size = 10;
 	char R[10] = {'1','a','Z','a','d','d','c','b','G','a'};
 	int s_size = 216;
 	char S[216] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
@@ -24,7 +36,7 @@ int main(void){
 	for(int i=0 ; i<216 ; i++){
 		printf("%c ",S[i]);
 	}
-
+*/
 /*	//test_case_1
 	//Table R
 	int r_size = 10;
@@ -38,12 +50,12 @@ int main(void){
 	}*/
 
 
-/*	//test_case_2
+	//test_case_2
 	//Table R
 	int r_size = 16;
 	char R[16] = {'0','1','2','3','4','5','6','7','8','9',':',';','<', '=','>','?'};
 	int s_size = 16;
-	char S[16] = {'9','8','7','6','5','4','3','2','1','0',':',';','<','=','>','?'};*/
+	char S[16] = {'9','8','7','6','5','4','3','2','1','0',':',';','<','=','>','?'};
 
 
 /*	//test_case_7
@@ -200,13 +212,141 @@ int main(void){
     }
 
 
-    res = RadixHashJoin(&relR, &relS);
-    print_result(res);
+    //res = RadixHashJoin(&relR, &relS);
+    //print_result(res);
 
     free(relR.tuples);
     free(relS.tuples);
 
-    free_result(res);
+    // free_result(res);
+
+    /*###########################################################################*/
+    /*###########################################################################*/
+    /*#################										#####################*/
+    /*#############												#################*/
+    /*##########				WELCOME TO 	PART 2				   ##############*/
+    /*#############												#################*/
+    /*#################										#####################*/
+    /*###########################################################################*/
+    /*###########################################################################*/
+
+    FILE *file;
+
+	char  ch;
+	// int i;
+	int num_of_files = 0;
+	char **files;
+	char nl = '\n';
+	size_t size = 5;
+
+	char file_init[100];
+	
+	strcpy(file_init, PATH);
+	strcat(file_init, "small.init");
+
+
+	if((file = fopen(file_init, "r")) == NULL){
+		printf("Error opening the file!\n");
+		exit(-1);
+	}
+
+	while((ch = fgetc(file)) != EOF){
+		if(ch == '\n')
+			num_of_files++;
+	}
+	printf("Num of files: %d\n", num_of_files);	
+
+	///////////////////////malloc checking?
+	files = (char **)malloc(num_of_files * sizeof(char *));
+	for(i = 0; i < num_of_files; i++){
+		files[i] = (char*)malloc(size * sizeof(char));
+	}
+	
+	fseek(file, 0, SEEK_SET); //Go to the beginning of the file
+	i=0;
+
+	char ptr[size];
+
+	while((fgets(ptr, size, file)) != NULL){
+		strcpy(files[i], ptr);
+		files[i] = strtok(files[i], &nl);
+		i++;
+	}
+
+	printf("FILES:\t");	
+	for(i = 0; i < num_of_files; i++){
+		 printf("%s ", files[i]);
+	}
+	printf("\n\n");
+
+	fclose(file);
+
+	//info about the relations
+	struct file_info info[num_of_files]; 
+
+	char fl[100];
+	strcpy(fl, PATH);
+
+	uint64_t **data; //pointers to the data addresses
+	data = (uint64_t**)malloc(num_of_files * sizeof(uint64_t*));
+
+	// uint64_t *data = (uint64_t*)mmap(NULL, buf.st_size, PROT_READ, MAP_PRIVATE | MAP_POPULATE, file_no, 0);
+
+	struct stat buf; //struct thats keep information of a file
+	int file_no = -1;
+
+
+	/*Storing relations in memory*/
+	for(i = 0; i < num_of_files; i++){
+		strcat(fl, files[i]);
+		// printf("%s\n", files[i]);
+		if((file = fopen(fl, "rb")) == NULL){
+			printf("Error opening the file 2!\n");
+			exit(-1);
+		}
+		strcpy(fl,PATH);
+
+		file_no = fileno(file); //return file descriptor of file
+		fstat(file_no, &buf);	//return file information inside buf
+
+		//store the relation into the memory
+		data[i] = (uint64_t*)mmap(NULL, buf.st_size, PROT_READ, MAP_PRIVATE | MAP_POPULATE, file_no, 0);
+
+		//////////////////////////////////////////////
+		//keep metadata for the relations
+		info[i].num_tup = *data[i];
+		info[i].num_col = *(data[i]+1);
+		create_col_array(&info[i], data[i]);
+
+		// printf("#####>%llu, %llu\n", info[i].num_tup, info[i].num_col);
+		printf("\n%s\tTuples: %llu\t -Columns: %llu\n", files[i], info[i].num_tup, info[i].num_col);
+
+		fclose(file);
+	}
+	//////////////////////////////
+
+	
+	// fclose(file);
+/*	printf("------------------------------------------------\n");
+	for(i = 0; i < num_of_files; i++){
+
+		printf("1.%llu\t", *info[i].col_array[0]);
+		printf("2.%llu\t", *info[i].col_array[1]);
+		printf("3.%llu\n", *info[i].col_array[2]);
+
+	}
+	printf("------------------------------------------------\n");*/
+
+	/*Free*/
+	free(data);
+
+	for(i = 0; i < num_of_files; i++){
+		free(info[i].col_array);
+	}
+	for(i=0; i < num_of_files; i++){
+		free(files[i]);
+	}
+	free(files);
 
     printf("End of Program.\n");
 	return 0;
